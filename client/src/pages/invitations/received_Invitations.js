e2142d61140db0177d42e9a7862e840cdc2f907d
ryan-bytes <thesoulsreaper15@gmail.com>
2022-12-16 17:56:31 +0100

    now clients can accept or decline invitations in the invitation page

--- client/src/pages/invitations/received_Invitations.js
+++ client/src/pages/invitations/received_Invitations.js
@@ -2,7 +2,52 @@ import { Link } from "react-router-dom";
 
 import empty_profile from "../../assets/images/emptyProfile.png";
 
+import postData from "../../utils/postData";
+
 export default function ReceivedInvitations({invitations}){
+
+    async function acceptUser(ev,acceptedUerID){
+        let buttons = ev.target.parentElement.querySelectorAll("input");
+        //disable buttons
+        buttons.forEach((item) => item.disabled = true);
+        
+        //send a request to the server for accepting this user
+        let data = {
+            "accepted_userID": acceptedUerID
+        }
+        let response = await postData("/user/invitations/accept",data);
+        if (response.ok){
+            //delete user from the invitation page
+            ev.target.parentElement.parentElement.remove();
+        }else{
+            //show error 
+
+            //enable buttons
+            buttons.forEach((item) => item.disabled = false);
+        }
+    }
+
+    async function declineUser(ev,declinedUerID){
+        let buttons = ev.target.parentElement.querySelectorAll("input");
+        //disable buttons
+        buttons.forEach((item) => item.disabled = true);
+        
+        //send a request to the server for accepting this user
+        let data = {
+            "declined_userID": declinedUerID
+        }
+        let response = await postData("/user/invitations/decline",data);
+        if (response.ok){
+            //delete user from the invitation page
+            ev.target.parentElement.parentElement.remove();
+        }else{
+            //show error 
+
+            //enable buttons
+            buttons.forEach((item) => item.disabled = false);
+        }
+    }
+
     function renderInvitations(invitations){
         let elementsArray = [];
         for (const id in invitations){
@@ -20,8 +65,16 @@ export default function ReceivedInvitations({invitations}){
                             {invitations[id].username}
                         </Link>
                         <div className="invitation-buttons">
-                            <input type="button" className="invitation-accept" value="Accept" />
-                            <input type="button" className="invitation-decline" value="Decline" />
+                            <input 
+                                type="button" 
+                                className="invitation-accept" 
+                                value="Accept"
+                                onClick={(ev) => acceptUser(ev,id)} />
+                            <input 
+                                type="button" 
+                                className="invitation-decline" 
+                                value="Decline" 
+                                onClick={(ev) => declineUser(ev,id)} />
                         </div>
                     </div>
             )
