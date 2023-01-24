9236cad3e56d1f328cb3fd714a784b8cd94c2ce0
ryan-byte <thesoulsreaper15@gmail.com>
2023-01-24 19:02:55 +0100

    add: post likes in the client side

--- client/src/components/userContent/UserPost.js
+++ client/src/components/userContent/UserPost.js
@@ -1,16 +1,59 @@
-import { memo } from "react";
+import { memo, useState, useEffect } from "react";
 
 import timePassedBy from "../../utils/timePassedBy";
 import empty_profile from "../../assets/images/emptyProfile.png";
 import { ReactComponent as Like } from  '../../assets/svg/like.svg';
 import { ReactComponent as Comment } from  '../../assets/svg/comment.svg';
-import { ReactComponent as Share } from  '../../assets/svg/share.svg';
 
+import postData from "../../utils/postData";
+import { getLoginCookieData } from "../../utils/accessPage";
 
 //used memo export so the parent wont keep rerendering this component on every change
 export default memo(UserPost);
 
+/**
+ * makes a request to add and remove like of a post
+ * @returns request object
+ */
+async function likeRequest(postID){
+    let data = {
+        postID
+    };
+    return await postData("/user/post/like",data,"PUT");
+}
+
+
 function UserPost({postData,username}){
+    const [totalLikes,setTotalLikes] = useState(postData.likes ? postData.likes.total: 0);
+    const [alreadyLike,setAlreadyLike] = useState(false);
+    const [postID] = useState(postData._id);
+
+    async function likePressed(ev){
+        let element = ev.currentTarget;
+        if (element.classList.contains("active-like")){
+            element.classList.remove("active-like");
+            setTotalLikes(totalLikes-1);
+            //make a request
+            await likeRequest(postID);
+        }else{
+            element.classList.add("active-like");
+            setTotalLikes(totalLikes+1);
+            //make a request
+            await likeRequest(postID);
+        }
+    }
+
+    useEffect(()=>{
+        //make the like active if the use already liked it
+        //get the userID
+        let userID = getLoginCookieData().userID;
+        //check if the userID is in the likes.users
+        let userAlreadyLikedPost = postData.likes ? userID in postData.likes.users: false; 
+        //activate like button if exists
+        if (userAlreadyLikedPost) setAlreadyLike(true);
+
+    },[setAlreadyLike,postData])
+
     return(
         <div>
             <div className="user-post-container">
@@ -37,14 +80,15 @@ function UserPost({postData,username}){
                 </div>
                 <hr />
                 <div className="post-interaction-container">
-                    <div>
-                        <Like className="post-interaction-item"/>
+                    <div className="button">
+                        {totalLikes}
+                        <Like className={alreadyLike ? "post-interaction-item active-like": "post-interaction-item"} 
+                            onClick={(ev) => {
+                                likePressed(ev)
+                        }}/>
                     </div>
                     <div>
                         <Comment className="post-interaction-item"/>
-                    </div>
-                    <div>
-                        <Share className="post-interaction-item"/>
                     </div>
                 </div>
             </div>
@@ -52,3 +96,5 @@ function UserPost({postData,username}){
         </div>
     )
 }
+
+
