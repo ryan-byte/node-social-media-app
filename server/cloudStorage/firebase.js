96e680c003552240c4098674900cbb56a38dd7a4
ryan-byte <thesoulsreaper15@gmail.com>
2023-02-05 15:19:42 +0100

    add: firebase storage image delete

--- server/cloudStorage/firebase.js
+++ server/cloudStorage/firebase.js
@@ -2,8 +2,8 @@ const path = require("path");
 const admin = require("firebase-admin");
 const {v4: uuidv4} = require('uuid');
 
-require('dotenv').config({path: firebaseEnvPath});
 const firebaseEnvPath = path.resolve('./firebase.env');
+require('dotenv').config({path: firebaseEnvPath});
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
@@ -36,6 +36,15 @@ async function uploadImage(fileBase64, fileName) {
     }
 }
 
+async function deleteImage(fileName){
+    const storageImagesRef = bucket.file(`${imageDir}/${fileName}`);
+    try {
+        await storageImagesRef.delete();
+    } catch (err) {
+        return {error: err};
+    }
+}
+
 async function getImageUrl(fileName,uuid) {
     try {
         return `https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/${encodeURIComponent(imageDir + "/" + fileName)}?alt=media&token=${uuid}`;
@@ -44,4 +53,4 @@ async function getImageUrl(fileName,uuid) {
     }
 };
 
-module.exports = {uploadImage}
+module.exports = {uploadImage, deleteImage}
